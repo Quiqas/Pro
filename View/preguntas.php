ab3298992414cc155398f139ec211c747b0a3b96
Cristhian <albert502@outlook.com>
2018-05-07 18:46:47 -0600

    Ignorar

--- View/preguntas.php
+++ View/preguntas.php
@@ -12,18 +12,13 @@
 
 
     $sql = "SELECT * FROM question";
-    $stmt = mysqli_stmt_init($connection);
-
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
-      echo "SQL Error";
-    }else{
-      mysqli_stmt_execute($stmt);
-      $result = mysqli_stmt_get_result($stmt);
+    
+      $result = mysqli_query($sql);
       $resultcheck = mysqli_num_rows($result);
 
      
 
-    }
+    
 ?>
 
 <div class="content-wrapper">	
--- View/static/Header.php
+++ View/static/Header.php
@@ -102,8 +102,6 @@ session_start();
             <li class="dropdown user user-menu">
               <!-- Menu Toggle Button -->
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <!-- The user image in the navbar-->
-                <img src="<?=$GLOBALS['COD']->dir?>/assets/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
                 <!-- hidden-xs hides the username on small devices so only the image appears. -->
                 <span class="hidden-xs"> <?php echo $_SESSION['user_name']?></span>
               </a>
